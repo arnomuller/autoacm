eccbc7163593f793d1a820fcd60a2dd53b75c5d0
arnomuller <arnomuller7@gmail.com>
2022-10-17 16:32:39 +0200

    tutoriel installation

---
+++ installation/Installer le package.R
@@ -0,0 +1,42 @@
+
+
+
+
+# 1) Installer les packages
+install.packages('shiny')
+install.packages('shinyWidgets')
+install.packages('shinyBS')
+install.packages('shinyjs')
+install.packages('shinythemes')
+install.packages('haven')
+install.packages('labelled')
+install.packages('FactoMineR')
+install.packages('factoextra')
+install.packages('GDAtools')
+install.packages('explor')
+install.packages('ade4')
+install.packages('fastcluster')
+install.packages('ggExtra')
+install.packages('DT')
+install.packages('ggrepel')
+install.packages('corrplot')
+install.packages('ggplot2')
+install.packages('forcats')
+install.packages('dplyr')
+install.packages('stringr')
+install.packages('tibble')
+install.packages('tidyr')
+install.packages('questionr')
+install.packages('readr')
+install.packages('ggiraph')
+
+# Installer le packages autoacm
+install.packages("D:/INED_dev/autoacm_0.0.0.9000.tar.gz", repos = NULL, type = "source")
+
+
+# Appeler le package
+library(autoacm)
+# Lancer la fonction :
+autoacm()
+
+
---
+++ installation/Lancer autoacm.R
@@ -0,0 +1,4 @@
+library(autoacm)
+autoacm()
+
+
